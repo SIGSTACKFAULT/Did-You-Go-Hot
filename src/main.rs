3c08f89b0f33c1b7eb4d230808155fda60fdfa3b
Zagrest-fixes <hidden@nowhere.local>
2026-05-17 11:59:32 -0700

    Removed test roll from happening on close.

--- src/main.rs
+++ src/main.rs
@@ -26,83 +26,83 @@ fn main() {
     run_app();
 
 
-    let available_rollers = [
-        AvailabileShips {
-            ship: Ship {
-                hot: 301_200_000,
-                cold: 201_200_000,
-            },
-            max_num_out: 99,
-            max_used: 3,
-        },
-        AvailabileShips {
-            ship: Ship {
-                hot: 126_000_000,
-                cold: 26_000_000,
-            },
-            max_num_out: 99,
-            max_used: 1,
-        },
-        // AvailabileShips {
-        //     ship: Ship {
-        //         hot: 130_000_000,
-        //         cold: 768_000,
-        //     },
-        //     max_num_out: 99,
-        //     max_used: 1,
-        // },
-        // AvailabileShips {
-        //     ship: Ship {
-        //         hot: 279_000_000,
-        //         cold: 179_000_000,
-        //     },
-        //     max_num_out: 99,
-        //     max_used: 1,
-        // },
-    ];
+    // let available_rollers = [
+    //     AvailabileShips {
+    //         ship: Ship {
+    //             hot: 301_200_000,
+    //             cold: 201_200_000,
+    //         },
+    //         max_num_out: 99,
+    //         max_used: 3,
+    //     },
+    //     AvailabileShips {
+    //         ship: Ship {
+    //             hot: 126_000_000,
+    //             cold: 26_000_000,
+    //         },
+    //         max_num_out: 99,
+    //         max_used: 1,
+    //     },
+    //     // AvailabileShips {
+    //     //     ship: Ship {
+    //     //         hot: 130_000_000,
+    //     //         cold: 768_000,
+    //     //     },
+    //     //     max_num_out: 99,
+    //     //     max_used: 1,
+    //     // },
+    //     // AvailabileShips {
+    //     //     ship: Ship {
+    //     //         hot: 279_000_000,
+    //     //         cold: 179_000_000,
+    //     //     },
+    //     //     max_num_out: 99,
+    //     //     max_used: 1,
+    //     // },
+    // ];
 
-    let start_state = HoleState::Shrink;
+    // let start_state = HoleState::Shrink;
 
-    let num_polos = 4;
-    let rollers_out = RollersUsed::new();
-    let hole = HoleInfo::from_kg(3_300_000_000);
-    let state = RollState {
-        remaining_mass: hole.mass_range(start_state),
-        rollers_out,
-        max_size_range: hole.max_range,
-        highest_hole_state: start_state,
-        used_ships: RollersUsed::new(),
-    };
-    let priorities = Priorities::new(vec![
-        Quality::ROProbability,
-        Quality::AvgNumPasses,
-        Quality::MaxOut,
-    ])
-    .unwrap();
-    let num_gigabytes = 16;
-    let num_bytes = Some(num_gigabytes * 1024 * 1024 * 1024);
-    let mut plans = get_best_roll_chart(
-        &available_rollers,
-        state,
-        start_state,
-        &priorities,
-        num_bytes,
-        roll_calc::PolorizationGuide::UpTo(4),
-        0,
-    );
-    for plan in plans {
-        if let Some((chart, qualities)) = plan {
-            println!(
-                "{}% {} {} {}",
-                qualities.roll_out_probability * 100.,
-                qualities.average_num_passes,
-                qualities.max_num_out,
-                qualities.num_polorizations,
-            );
-        } else {
-            println!("None")
-        }
-    }
+    // let num_polos = 4;
+    // let rollers_out = RollersUsed::new();
+    // let hole = HoleInfo::from_kg(3_300_000_000);
+    // let state = RollState {
+    //     remaining_mass: hole.mass_range(start_state),
+    //     rollers_out,
+    //     max_size_range: hole.max_range,
+    //     highest_hole_state: start_state,
+    //     used_ships: RollersUsed::new(),
+    // };
+    // let priorities = Priorities::new(vec![
+    //     Quality::ROProbability,
+    //     Quality::AvgNumPasses,
+    //     Quality::MaxOut,
+    // ])
+    // .unwrap();
+    // let num_gigabytes = 16;
+    // let num_bytes = Some(num_gigabytes * 1024 * 1024 * 1024);
+    // let mut plans = get_best_roll_chart(
+    //     &available_rollers,
+    //     state,
+    //     start_state,
+    //     &priorities,
+    //     num_bytes,
+    //     roll_calc::PolorizationGuide::UpTo(4),
+    //     0,
+    // );
+    // for plan in plans {
+    //     if let Some((chart, qualities)) = plan {
+    //         println!(
+    //             "{}% {} {} {}",
+    //             qualities.roll_out_probability * 100.,
+    //             qualities.average_num_passes,
+    //             qualities.max_num_out,
+    //             qualities.num_polorizations,
+    //         );
+    //     } else {
+    //         println!("None")
+    //     }
+    // }
 
     // let mut file = File::create("plan.txt").unwrap();
     // file.write_all(chart.to_text_chart().as_bytes()).unwrap();
